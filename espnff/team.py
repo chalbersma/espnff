e8979c7fc2cdd232996ac4f4cb53f1edd777d67f
drewski3420 <drewski3420@gmail.com>
2017-09-10 08:57:07 -0400

    Added rosters and weekly scores
    team.get_roster returns weekly roster with projected and actual scores.

--- espnff/team.py
+++ espnff/team.py
@@ -1,6 +1,7 @@
+import requests
 class Team(object):
     '''Teams are part of the league'''
-    def __init__(self, data):
+    def __init__(self, data, league_id, year, cookies):
         self.team_id = data['teamId']
         self.team_abbrev = data['teamAbbrev']
         self.team_name = "%s %s" % (data['teamLocation'], data['teamNickname'])
@@ -15,6 +16,10 @@ class Team(object):
         self.schedule = []
         self.scores = []
         self.mov = []
+        self.league_id = league_id
+        self.cookies = cookies
+        self.year = year
+        self.ENDPOINT = "http://games.espn.com/ffl/api/v2/"
         self._fetch_schedule(data)
 
     def __repr__(self):
@@ -39,7 +44,44 @@ class Team(object):
             self.scores.append(score)
             self.schedule.append(opponentId)
 
-    def get_roster(self, week):
-        '''Get roster for a given week'''
-        roster = None
+    def get_roster(self,week=None):
+        params = {
+            'leagueId': self.league_id,
+            'seasonId': self.year,
+            'teamIds': self.team_id
+        }
+        roster_slots = {0:'QB',2:'RB',4:'WR',6:'TE',23:'FLEX',16:'D/ST',17:'K',20:'Bench'}
+        if week is not None:
+            params['scoringPeriodId'] = week
+        r = requests.get('%srosterInfo' % (self.ENDPOINT, ), params=params, cookies=self.cookies)
+        data = r.json()      
+      
+        players = data['leagueRosters']['teams'][0]['slots']
+        roster = []
+        for p in players:
+            if 'player' in p:
+                player_name = ('%s %s' %(p['player']['firstName'],p['player']['lastName']))
+                position = roster_slots[p['slotCategoryId']]
+                player_id = p['player']['playerId']
+                params = {
+                    'leagueId' : self.league_id,
+                    'seasonId' : self.year,
+                    'teamIds' : self.team_id,
+                    'playerId' : player_id,
+                    'useCurrentPeriodRealStats' : 'true',
+                    'useCurrentPeriodProjectedStats' : 'true'
+                }
+                if week is not None:
+                    params['scoringPeriodId'] = week
+                r = requests.get('%splayerInfo' % (self.ENDPOINT, ), params=params, cookies=self.cookies)
+                data = r.json()
+                if 'appliedStatTotal' in data['playerInfo']['players'][0]['currentPeriodRealStats']:
+                    player_score = data['playerInfo']['players'][0]['currentPeriodRealStats']['appliedStatTotal']
+                else:
+                    player_score = 0
+                if 'appliedStatTotal' in data['playerInfo']['players'][0]['currentPeriodProjectedStats']:
+                    projected_score = data['playerInfo']['players'][0]['currentPeriodProjectedStats']['appliedStatTotal']
+                else:
+                    projected_score = 0
+                roster.append({'name':player_name,'position':position,'player_id':player_id,'actual score':player_score, 'projected_score':projected_score})
         return roster
